d21864e0d6aae2bda686acfc2a3de4ca8810f7f3
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-03-26 20:19:13 -0300

    feat: adicionando mailhog localmente para receber emails

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+Route::get('/email', function () {
+
+    Mail::raw('Mensagem de teste rh mangnt', function (Message $message) {
+        $message->to('teste@gmail.com')
+            ->subject('Bem-vindo ao RH Mangnt')
+            ->from('noreply@rhmangnt.com');
+    });
+
+    echo "Email enviado com sucesso!";
 });
