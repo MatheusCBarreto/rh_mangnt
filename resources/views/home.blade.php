62d353e0a6c83abce20be015b5adc882cf40d75d
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-04-10 19:31:18 -0300

    feat: criando tela de login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layout-app page-title="Home">
     <h1>Bem-vindo ao sistema de gestão de recursos humanos!</h1>
     <p>Este é o painel inicial onde você pode acessar as principais funcionalidades do sistema.</p>
+
+    @php
+        dump(auth()->user());
+    @endphp
 </x-layout-app>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
 
+    Route::redirect('/', '/home');
     Route::get('/home', function () {
         return view('home');
     })->name('home');
