b66b3b0553e8b212b2e2cc8ddbfbdfdd89773e0c
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-04-10 19:51:59 -0300

    feat: criando a UserBar e a função logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,3 @@
 <x-layout-app page-title="Home">
-    <h1>Bem-vindo ao sistema de gestão de recursos humanos!</h1>
-    <p>Este é o painel inicial onde você pode acessar as principais funcionalidades do sistema.</p>
-
-    @php
-        dump(auth()->user());
-    @endphp
+    
 </x-layout-app>
