f6093c7278a767c3424974231f042f8f44a6badc
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-04-13 21:41:46 -0300

    feat: sidebar construída

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,26 @@
+<div class="d-flex flex-column sidebar pt-4">
+
+    <a href="{{ route('home') }}"><i class="fas fa-home me-3"></i>Home</a>
+
+    @can('admin')
+        <a href="#" class=""><i class="fas fa-users me-3"></i>Colaborators</a>
+        <a href="#" class=""><i class="fa-solid fa-user-gear me-3"></i>RH Colaborators</a>
+        <a href="#" class=""><i class="fa-solid fa-industry me-3"></i>Departments</a>
+    @endcan
+
+    <hr>
+
+    <a href="#" class=""><i class="fas fa-cog me-3"></i>User profile</a>
+
+    <hr>
+
+    {{-- logout --}}
+    <div class="text-center mt-3">
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button tupe="submit" class="btn bnt-sm btn-outline-dark">
+                <i class="fas fa-sign-out-alt me-3"></i>Logout
+            </button>
+        </form>
+    </div>
+</div>
