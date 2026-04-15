8b663dd65779794ee0ac52b507feec8bdb42cabc
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-04-15 18:43:45 -0300

    feat: criando funcionalidade de alterar senha.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,34 +4,61 @@
 
         <hr>
 
-        <div class="d-flex gap-5">
-            <div>
-                <i class="fa-solid fa-user me-3">
-                    {{ auth()->user()->name }}
-                </i>
-            </div>
-
-            <div>
-                <i class="fa-solid fa-user me-3">
-                    {{ auth()->user()->role }}
-                </i>
-            </div>
-
-            <div>
-                <i class="fa-solid fa-at me-3">
-                    {{ auth()->user()->email }}
-                </i>
-            </div>
-
-            <div>
-                <i class="fa-solid fa-calendar-days me-3">
-                    {{ auth()->user()->created_at->format('d/m/Y') }}
-                </i>
-            </div>
-
-        </div>
+        <x-profile-user-data />
 
         <hr>
+
+        <div class="container-fluid m-0 p-0 mt-5">
+            <div class="row">
+                <div class="col-3">
+                    <div class="border p-5 shadow-sm">
+                        <form action="#" method="post">
+                            @csrf
+                            <h3>Change password</h3>
+
+                            <div class="mb-3">
+                                <label for="current_password" class="form-label">Current password</label>
+                                <input type="password" name="current_password" id="current_password"
+                                    class="form-control">
+                                @error('current_password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="new_password" class="form-label">New password</label>
+                                <input type="password" name="new_password" id="new_password" class="form-control">
+                                @error('new_password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="new_password_confirmation" class="form-label">Confirm new password</label>
+                                <input type="password" name="new_password_confirmation" id="new_password_confirmation"
+                                    class="form-control">
+                                @error('new_password_confirmation')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Change password</button>
+                            </div>
+
+                        </form>
+                        @if (session('error'))
+                            <div class="alert alert-danger mt-3">{{ session('error') }}</div>
+                        @endif
+
+                        @if (session('success'))
+                            <div class="alert alert-success mt-3">{{ session('success') }}</div>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </x-layout-app>
