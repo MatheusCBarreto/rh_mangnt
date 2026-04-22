9abf9fa6712a4b8060de0cc66bb1391d7b9d98c4
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-04-22 19:50:53 -0300

    feat: page de boas-vindas criada com sucesso.

--- app/Http/Controllers/ConfirmAccountController.php
+++ app/Http/Controllers/ConfirmAccountController.php
@@ -36,6 +36,6 @@ class ConfirmAccountController extends Controller
         $user->email_verified_at = now();
         $user->save();
 
-        return redirect()->route('login');
+        return view('auth.welcome', ['user' => $user]);
     }
 }
